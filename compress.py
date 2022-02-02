5e51cc78751ad5bfb20534063aa8eadc25df5962
Shashank Rajput <shashankrajput@Shashanks-MacBook-Pro.local>
2022-02-02 12:32:17 -0600

    Added code for pruning based compression

--- compress.py
+++ compress.py
@@ -2,6 +2,7 @@ import os
 from argparse import ArgumentParser
 
 import torch
+import torch.nn.utils.prune as prune
 
 from pytorch_lightning import Trainer, seed_everything
 # from pytorch_lightning.callbacks import ModelCheckpoint
@@ -13,42 +14,7 @@ from data import CIFAR10Data
 from module import CIFAR10Module
 
 
-def compress(args):
-    seed_everything(0)
-    # os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
 
-    if args.logger == "wandb":
-        logger = WandbLogger(name=args.classifier, project="cifar10")
-    elif args.logger == "tensorboard":
-        logger = TensorBoardLogger("cifar10", name=args.classifier)
-
-    # checkpoint = ModelCheckpoint(monitor="acc/val", mode="max", save_last=False)
-
-    trainer = Trainer(
-        fast_dev_run=bool(args.dev),
-        logger=logger if not bool(args.dev + args.test_phase) else None,
-        # gpus=-1,
-        deterministic=True,
-        # weights_summary=None,
-        enable_model_summary=False,
-        log_every_n_steps=1,
-        max_epochs=args.max_epochs,
-        # checkpoint_callback=checkpoint,
-        precision=args.precision,
-    )
-
-    model = CIFAR10Module(args)
-    data = CIFAR10Data(args)
-
-    state_dict = os.path.join(
-                "cifar10_models", "state_dicts", args.classifier + ".pt"
-            )
-    model.model.load_state_dict(torch.load(state_dict))
-    model.model.eval() # for evaluation
-
-    trainer=Trainer()
-
-    trainer.test(model, data.test_dataloader())
 
 
 
@@ -66,7 +32,7 @@ if __name__ == "__main__":
     )
 
     # TRAINER args
-    parser.add_argument("--classifier", type=str, default="resnet18")
+    parser.add_argument("--classifier", type=str, default="vgg11_bn")
     parser.add_argument("--pretrained", type=int, default=0, choices=[0, 1])
 
     parser.add_argument("--precision", type=int, default=32, choices=[16, 32])
@@ -79,4 +45,76 @@ if __name__ == "__main__":
     parser.add_argument("--weight_decay", type=float, default=1e-2)
 
     args = parser.parse_args()
-    compress(args)
+
+
+
+    seed_everything(0)
+    # os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
+
+    if args.logger == "wandb":
+        logger = WandbLogger(name=args.classifier, project="cifar10")
+    elif args.logger == "tensorboard":
+        logger = TensorBoardLogger("cifar10", name=args.classifier)
+
+    
+    trainer = Trainer(
+        fast_dev_run=bool(args.dev),
+        logger=logger if not bool(args.dev + args.test_phase) else None,
+        # gpus=-1,
+        deterministic=True,
+        enable_model_summary=False,
+        log_every_n_steps=1,
+        max_epochs=args.max_epochs,
+        precision=args.precision,
+    )
+
+    model = CIFAR10Module(args)
+    data = CIFAR10Data(args)
+
+    state_dict = os.path.join(
+                "cifar10_models", "state_dicts", args.classifier + ".pt"
+            )
+    model.model.load_state_dict(torch.load(state_dict))
+
+    # model.model.eval() # for evaluation
+    # trainer=Trainer()
+    # trainer.test(model, data.test_dataloader())
+    
+    
+    parameters_to_prune=[]
+    for module in model.model.modules():
+        if isinstance(module, torch.nn.Conv2d) or isinstance(module, torch.nn.Linear):
+            parameters_to_prune.append((module, 'weight'))
+    
+
+    torch.save(model.model.state_dict(), './compressed_models/vgg11_bn_100.pt')
+
+    prune_fraction=0.1
+    current=100
+
+    sparsities=[]
+    for (module, name) in parameters_to_prune:
+        sparsities.append(100. * float(torch.sum(module.weight == 0)) / float(module.weight.nelement()))
+        
+    print(current)
+    print(sparsities)
+    
+    while current>1:
+        current=current*(1-prune_fraction)
+        prune.global_unstructured(
+            parameters_to_prune,
+            pruning_method=prune.L1Unstructured,
+            amount=(100-current)/100,
+        )
+        sparsities=[]
+        for (module, name) in parameters_to_prune:
+            prune.remove(module, name)
+            sparsities.append(100. * float(torch.sum(module.weight == 0)) / float(module.weight.nelement()))
+        
+        print(current)
+        print(sparsities)
+        
+        
+        torch.save(model.model.state_dict(), './compressed_models/vgg11_bn_'+str(int(current))+'.pt')
+
+    
